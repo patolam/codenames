583f65abf3e2e23f93aa1e6a43da7e7c0c1681aa
Marcin Patoła <marcin.patola@boldare.com>
2020-03-19 16:22:56 +0100

    initial commit

---
+++ apps/codenames/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: 'codenames',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/codenames',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js'
+  ]
+};
