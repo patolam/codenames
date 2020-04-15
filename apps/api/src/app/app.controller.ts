696a9d2f28ea77a44f53da450261e9599a3dd55f
Marcin Patoła <marcin.patola@boldare.com>
2020-04-15 11:41:41 +0200

    Share fix

--- apps/api/src/app/app.controller.ts
+++ apps/api/src/app/app.controller.ts
@@ -1,5 +1,8 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Res } from '@nestjs/common';
 import { StateService } from './state/state.service';
+import * as fs from 'fs';
+import * as path from 'path';
+import { Response } from 'express';
 
 @Controller()
 export class AppController {
@@ -16,5 +19,13 @@ export class AppController {
   @Get('/id')
   getId(): { id: string } {
     return this.stateService.getRandId();
+  }
+
+  @Get('/screen')
+  getScreen(@Res() res: Response) {
+    const data = fs.readFileSync(path.resolve(__dirname, 'assets/screen.png'));
+
+    res.set('Content-Type', 'image/png');
+    return res.status(HttpStatus.OK).send(Buffer.from(data));
   }
 }
--- apps/codenames/src/app/board/dialogs/dialog-end-game/dialog-end-game.component.ts
+++ apps/codenames/src/app/board/dialogs/dialog-end-game/dialog-end-game.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { BoardState } from '../../../../../../shared/model/state';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { TranslateService } from '@ngx-translate/core';
+import { environment } from '../../../../environments/environment';
 
 @Component({
   selector: 'cdn-dialog-end-game',
@@ -25,6 +26,21 @@ export class DialogEndGameComponent implements OnInit {
   getWinnerTranslate(): any {
     return {
       value: this.translate.instant(`common.team.${this.state?.game?.winner}`)
-    }
+    };
+  }
+
+  shareClick(): void {
+    this.showPopup(`https://www.facebook.com/sharer/sharer.php?
+      u=http://playkeywords.pl
+      &quote=Keywords - zespołowa gra słowna online
+      &picture=${environment.api}/screen`,
+      300, 600);
+  }
+
+  private showPopup(url: string, width: number, height: number): void {
+    const left = (screen.width / 2) - (width / 2);
+    const top = (screen.height / 2) - (height / 2);
+
+    window.open(url, 'popup', `width=${width}, height=${width}, top=${top}, left=${left}`);
   }
 }
