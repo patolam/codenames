d38221fb1b2afef99d302d745e50ff9e4d9f91a5
Marcin Patoła <marcin.patola@boldare.com>
2020-04-19 15:51:24 +0200

    Time bottom line

--- apps/codenames/src/app/board/components/toolbar/toolbar.component.ts
+++ apps/codenames/src/app/board/components/toolbar/toolbar.component.ts
@@ -1,12 +1,15 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, Output, SimpleChanges } from '@angular/core';
 import { BoardState } from '../../../../../../shared/model/state';
 import { timer } from 'rxjs';
 import * as moment from 'moment';
 import { FormGroup } from '@angular/forms';
 
+const DEFAULT_TIMER = 90;
+
 interface Timer {
-  min?: string,
-  sec?: string
+  min?: string;
+  sec?: string;
+  line?: number;
 }
 
 @Component({
@@ -14,22 +17,23 @@ interface Timer {
   templateUrl: './toolbar.component.html',
   styleUrls: ['./toolbar.component.scss']
 })
-export class ToolbarComponent {
+export class ToolbarComponent implements OnChanges {
   @Input() state: BoardState;
   @Input() clientId: string;
   @Input() playerForm: FormGroup;
-
   @Output() startGame: EventEmitter<void> = new EventEmitter();
+
   @Output() stopGame: EventEmitter<void> = new EventEmitter();
   @Output() clearScore: EventEmitter<void> = new EventEmitter();
   @Output() setTimer: EventEmitter<void> = new EventEmitter();
   @Output() upTimer: EventEmitter<void> = new EventEmitter();
 
   time: Timer = {};
+  lineStyle = {};
 
-  counter = timer(0, 200)
+  counter = timer(0, 500)
     .subscribe(() => {
-      const now = moment();
+      const now = moment().add(-1, 's');
       const start = moment(this.state?.game?.current?.timer);
 
       const minDiff = start.diff(now, 'm');
@@ -39,11 +43,26 @@ export class ToolbarComponent {
         if (minDiff >= 0 && secDiff >= 0) {
           this.time = {
             min: minDiff.toString(),
-            sec: (secDiff >= 10 ? secDiff : `0${secDiff}`).toString()
+            sec: (secDiff >= 10 ? secDiff : `0${secDiff}`).toString(),
+            line: start.diff(now, 's') / DEFAULT_TIMER
+          };
+
+          this.lineStyle = {
+            width: 'calc(100vw * ' + this.time?.line + ')'
           };
         } else {
           this.upTimer.emit();
         }
       }
     });
+
+  ngOnChanges(changes: SimpleChanges): void {
+    const prev: BoardState = changes.state.previousValue;
+    const curr: BoardState = changes.state.currentValue;
+
+    if (!prev.game.current.timer && curr.game.current.timer) {
+      this.time = {};
+      this.lineStyle = {};
+    }
+  }
 }
