43cd3e644604e04a5be4263addd9abe416c65bd7
marcinpatola <marcin.patola@boldare.com>
2021-02-11 11:34:08 +0100

    Fix app path

--- angular.js
+++ angular.js
@@ -2,13 +2,13 @@ const express = require('express');
 const http = require('http');
 const path = require('path');
 
-const app = express();
+const appPath = __dirname + '/dist/apps/codenames';
 
+const app = express();
 const port = process.env.PORT || 80;
 
-app.use(express.static(__dirname + '/dist/apps/codenames'));
-
-app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
+app.use(express.static(appPath));
+app.get('/*', (req, res) => res.sendFile(path.join(appPath + '/index.html')));
 
 const server = http.createServer(app);
 
